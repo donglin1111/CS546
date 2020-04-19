7d621f693d389f1ca911785471e1c98a96747d91
donglin1111 <407218800@qq.com>
2020-04-19 04:51:32 -0400

    homepages

--- code/routes/index.js
+++ code/routes/index.js
@@ -1,16 +1,16 @@
-const postRoutes = require('./posts');
+const homepageRoutes = require('./homepage');
 const userRoutes = require('./users');
 const path = require('path');
 
 const constructorMethod = (app) => {
-    app.use('/posts', postRoutes);
+    app.use('/homepage', homepageRoutes);
     app.use('/users', userRoutes);
     app.get('/about', (req, res) => {
         res.sendFile(path.resolve('static/about.html'));
     });
 
     app.use('*', (req, res) => {
-        res.redirect('/posts');
+        res.redirect('/homepage');
     });
 };
 
